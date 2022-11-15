ca0bcf71af28093e4d18e5da48cbdad0d7bb53a5
Vika <Vikasatta1998@gmail.com>
2022-11-15 11:29:03 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,19 @@ root.render(
   </React.StrictMode>
 );
 
+let arr = [2,2,2,2]
+function findAverage(array:Array<number>) {
+    let arrayL = array.length
+    let sum =0
+    for(let i=0;i < arrayL;i++){
+        sum+=array[i]
+    }
+
+    let res = sum/arrayL
+    console.log(res)
+    return res
+}
+findAverage(arr)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
